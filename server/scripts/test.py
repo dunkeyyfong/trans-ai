71f657b5cb0349fa71623d7c3fdf3f6c4f30d70c
dunkeyyfong <phongtrong865@gmail.com>
2024-12-21 17:56:13 +0700

    update server

--- server/scripts/test.py
+++ server/scripts/test.py
@@ -1,4 +1,16 @@
+from openai import OpenAI
+from dotenv import load_dotenv
 import os
-import openai
-openai.api_key = os.getenv('OPENAI_KEY')
-print(openai.Model.list())
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_KEY")
+
+client = OpenAI(api_key=api_key)
+
+# Lấy danh sách models
+response = client.models.list()
+
+# In danh sách models
+for model in response.data:
+    print(f"Model ID: {model.id}")
