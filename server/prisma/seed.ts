80be23e1a7b5ba8b14301ffd47825c742ef22b47
dunkeyyfong <phongtrong865@gmail.com>
2025-01-22 13:25:16 +0700

    add verifyToken field to Admin user in seed script

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -8,9 +8,10 @@ async function main() {
     data: {
       name: 'Admin',
       email: 'admin@transai.site',
-      password: '$2a$12$.KE88GSEPuO5x.XGeG8qh.XL3ittFwN3ndGUDqqZ62ZWZFlh2d/P2',
+      password: '$2a$12$.KE88GSEPuO5x.XGeG8qh.XL3ittFwN3ndGUDqqZ62ZWZFlh2d/P2', //Admin@123
       isActive: true,
-      role: 'ADMIN'
+      role: 'ADMIN',
+      verifyToken: 'eyJhbGciOiJIUzI1NiJ9.YWRtaW5AdHJhbnNhaS5zaXRl.2P6eaHjkipMGef88Dz3tbsrBFw1BJSAPZ2RLq4d7J2E'
     }
   })
   console.log('New User:', newUser)
