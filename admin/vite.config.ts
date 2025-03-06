8492fca1f462db4da23e90b31ec44dd5aea5ca89
dunkeyyfong <phongtrong865@gmail.com>
2025-03-06 10:19:54 +0700

    Add admin frontend and backend services to docker-compose and update project configuration

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  esbuild: {
+    target: "esnext",
+    platform: "node",
+  },
+  server: {
+    allowedHosts: ["localhost", "admin.kotoba.tokyo"],
+    host: "0.0.0.0",
+    port: 5173,
+  },
+});
