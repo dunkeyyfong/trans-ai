46e222866538af671048e611ff7e9c7c6535f4fe
dunkeyyfong <phongtrong865@gmail.com>
2025-03-07 22:49:42 +0700

    Upgrade admin dashboard
    - Update project to Tailwind CSS v4
    - Refactor project structure and dependencies
    - Replace default Vite template with comprehensive dashboard layout
    - Add new UI components and routing
    - Update favicon and project metadata
    - Integrate additional libraries for enhanced functionality

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        // This will transform your SVG to a React component
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+    tsconfigPaths()
+  ],
   esbuild: {
     target: "esnext",
     platform: "node",
