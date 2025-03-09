5bfb3f77241478acdb08f1fac2c1d88b739f10e3
dunkeyyfong <phongtrong865@gmail.com>
2025-03-09 23:31:27 +0700

    fix up router

--- admin/src/components/common/PageBreadCrumb.tsx
+++ admin/src/components/common/PageBreadCrumb.tsx
@@ -5,6 +5,9 @@ interface BreadcrumbProps {
 }
 
 const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
+
+  const accessToken = localStorage.getItem("accessToken");
+
   return (
     <div className="flex flex-wrap items-center justify-between gap-3 mb-6">
       <h2
@@ -18,7 +21,7 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
           <li>
             <Link
               className="inline-flex items-center gap-1.5 text-sm text-gray-500 dark:text-gray-400"
-              to="/"
+              to={`/home?accessToken=${accessToken}`}
             >
               Home
               <svg
