3c3ac62191ba62e5978ed615cf9ed99d15843c09
Tung11010 <tungndbh01085@fpt.edu.vn>
2025-03-10 12:55:26 +0700

    update

--- admin/src/components/header/UserDropdown.tsx
+++ admin/src/components/header/UserDropdown.tsx
@@ -27,7 +27,7 @@ export default function UserDropdown() {
         onClick={toggleDropdown}
         className="flex items-center text-gray-700 dropdown-toggle dark:text-gray-400"
       >
-        <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
+        <span className="mr-3 overflow-hidden rounded-full h-8 w-8">
           <img src="/images/user/owner.jpg" alt="User" />
         </span>
 
