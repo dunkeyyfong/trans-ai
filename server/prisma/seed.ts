57e1dba56b4a4359f79bc040b891b0fc52c25395
dunkeyyfong <phongtrong865@gmail.com>
2025-03-26 10:04:48 +0700

    update seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -3,18 +3,25 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // Tạo một user mới
-  const newUser = await prisma.user.create({
-    data: {
-      name: 'Admin',
-      email: 'admin@kotoba.tokyo',
-      password: '$2a$12$.KE88GSEPuO5x.XGeG8qh.XL3ittFwN3ndGUDqqZ62ZWZFlh2d/P2', //Admin@123
-      isActive: true,
-      role: 'ADMIN',
-      verifyToken: 'eyJhbGciOiJIUzI1NiJ9.YWRtaW5AdHJhbnNhaS5zaXRl.2P6eaHjkipMGef88Dz3tbsrBFw1BJSAPZ2RLq4d7J2E'
-    }
-  })
-  console.log('New User:', newUser)
+  const existingUser = await prisma.user.findUnique({
+    where: { email: 'admin@kotoba.tokyo' },
+  });
+  if (!existingUser) {
+    // Tạo một user mới
+    const newUser = await prisma.user.create({
+      data: {
+        name: 'Admin',
+        email: 'admin@kotoba.tokyo',
+        password: '$2a$12$.KE88GSEPuO5x.XGeG8qh.XL3ittFwN3ndGUDqqZ62ZWZFlh2d/P2', //Admin@123
+        isActive: true,
+        role: 'ADMIN',
+        verifyToken: 'eyJhbGciOiJIUzI1NiJ9.YWRtaW5AdHJhbnNhaS5zaXRl.2P6eaHjkipMGef88Dz3tbsrBFw1BJSAPZ2RLq4d7J2E'
+      }
+    })
+    console.log('New User:', newUser)
+  } else {
+    console.log('Skip run seed')
+  }
 }
 
 main()
